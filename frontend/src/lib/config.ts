afa55813e4d4b16c25848259c8870aefa762921c
Shounak Joshi <shounakjoshi88@gmail.com>
2026-05-03 20:31:50 +0530

    Update BACKEND_URL to use environment variable

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3001";
+export const BACKEND_URL = process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3001";
 export const UNSPLASH_ACCESS_KEY = "F20HPbf6v1PbJuOyW4Q-b5vl5eflqsNzs-eIjoI8hf4";
