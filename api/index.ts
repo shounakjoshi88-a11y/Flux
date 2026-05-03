ef2c1f23026f91d50f348ebf4c8bb55cc9e426df
Shounak Joshi <shounakjoshi88@gmail.com>
2026-05-03 20:32:23 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,7 @@
-// api/index.ts (at the root of your repo, next to frontend/ and backend/)
 import app from "../backend/index";
-export default app;
+import express from "express";
+
+const serverless = express();
+serverless.use("/api", app);
+
+export default serverless;
